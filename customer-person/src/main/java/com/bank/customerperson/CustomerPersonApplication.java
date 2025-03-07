fabdfc6a97ecf332b59c911927a56e5a8367d3bd
Joel-x-x <acostajoel0001@gmail.com>
2025-03-06 20:17:55 -0500

    feat: add proyect

---
+++ customer-person/src/main/java/com/bank/customerperson/CustomerPersonApplication.java
@@ -0,0 +1,13 @@
+package com.bank.customerperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerPersonApplication.class, args);
+	}
+
+}
---
+++ customer-person/src/test/java/com/bank/customerperson/CustomerPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.customerperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
