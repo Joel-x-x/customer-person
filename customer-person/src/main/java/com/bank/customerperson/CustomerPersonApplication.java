56166c10936b495bb6117209d5f49a6a30b9e29f
Joel-x-x <acostajoel0001@gmail.com>
2025-03-09 16:54:45 -0500

    feat: add config docker

--- customer-person/src/main/java/com/bank/customerperson/CustomerPersonApplication.java
+++ customer-person/src/main/java/com/bank/customerperson/CustomerPersonApplication.java
@@ -2,7 +2,9 @@ package com.bank.customerperson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CustomerPersonApplication {
 
